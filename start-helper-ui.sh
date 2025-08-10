b403b2ac6f4d52c28ea039ab1710a7ce4b536837
josephaw1022 <josephsims1@gmail.com>
2025-08-10 16:13:26 -0400

    add name to helper ui

--- start-helper-ui.sh
+++ start-helper-ui.sh
@@ -4,7 +4,8 @@
 mkdir -p ./quay/config ./quay/storage
 
 # 2) Run config UI with required password
-podman run --rm -it \
+podman run --rm -itd \
+    --name quay-config-ui \
   -e CONFIG_APP_PASSWORD=admin123 \
   -p 8080:8080 \
   -v ./quay/config:/conf/stack:z \
